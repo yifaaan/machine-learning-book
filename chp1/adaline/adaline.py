8c2a802f6cba186b5f89eeb06185dac4a1ad80eb
lyfQAQ <0219lyf@gmail.com>
2025-01-26 10:27:45 +0800

    update

--- chp1/adaline/adaline.py
+++ chp1/adaline/adaline.py
@@ -1,7 +1,35 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+    # setup marker generator and color map
+    markers = ("o", "s", "^", "v", "<")
+    colors = ("red", "blue", "lightgreen", "gray", "cyan")
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    #plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    x1s = np.arange(x1_min, x1_max, resolution)
+    x2s = np.arange(x2_min, x2_max, resolution)
+    xx1, xx2 = np.meshgrid(x1s, x2s)
+    labs = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    labs = labs.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, labs, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot class examples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[ y==cl, 0],
+                    y=X[y == cl, 1],
+                    alpha=0.8,
+                    c=colors[idx],
+                    marker=markers[idx],
+                    label=f"Class {cl}",
+                    edgecolors="black")
 class AdalineGD:
     """ADAptive LIner NEuron classifier.
 
@@ -67,6 +95,9 @@ class AdalineGD:
         
     def activation(self, X):
         return X
+    
+    def predict(self, X):
+        return np.where(self.activation(self.net_input(X)) >= 0.5, 1 , 0)
 
 
 s = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
@@ -93,4 +124,31 @@ ax[1].set_xlabel('Epochs')
 ax[1].set_ylabel('Mean squared error')
 ax[1].set_title('Adaline - Learning rate 0.0001')
 
-plt.savefig("adaline")
+X_std = np.copy(X)
+X_std[:, 0] = (X_std[:, 0] - X_std[:, 0].mean()) / X_std[:, 0].std()
+X_std[:, 1] = (X_std[:, 1] - X_std[:, 1].mean()) / X_std[:, 1].std()
+ada3 = AdalineGD(eta=0.5, n_iter=20).fit(X_std, y)
+# ax[2].plot(range(1, len(ada3.losses_) + 1), ada3.losses_, marker='o')
+# ax[2].set_xlabel('Epochs')
+# ax[2].set_ylabel('Mean squared error')
+# ax[2].set_title('Adaline - Learning rate 0.5')
+
+plt.show()
+
+
+plot_decision_regions(X_std, y, classifier=ada3)
+plt.title('Adaline - Gradient descent')
+
+plt.xlabel('Sepal length [standardized]')
+
+plt.ylabel('Petal length [standardized]')
+
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+
+plt.plot(range(1, len(ada3.losses_) + 1), ada3.losses_, marker="o")
+plt.xlabel("Epochs")
+plt.ylabel("Mean squared error")
+plt.tight_layout()
+plt.show()
